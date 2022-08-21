ced9d1e9e49d9b60107dbe360aecd36958b7331e
bogdinamitawhoami <bogdifromsalem@yahoo.com>
2022-08-21 09:51:26 +0200

    Lab almost ready, need to fix image

---
+++ src/components/User.vue
@@ -0,0 +1,17 @@
+<template>
+    <span class="name"> {{ user.name }} </span>
+    <span class="handle">  {{ user.handle }} </span>
+</template>
+
+<script>
+
+export default {
+    props: {
+        user: {
+            name: String,
+            handle: String,
+        },
+    },
+};
+
+</script>
